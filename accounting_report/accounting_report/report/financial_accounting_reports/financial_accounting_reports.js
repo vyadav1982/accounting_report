cd50e733a2c309f60abf7883ccb82510f2e244a4
Vinay Yadav <vyadav1982@gmail.com>
2024-05-22 11:39:23 +0300

    feat: merge financial accounting reports

--- accounting_report/accounting_report/report/financial_accounting_reports/financial_accounting_reports.js
+++ accounting_report/accounting_report/report/financial_accounting_reports/financial_accounting_reports.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2024, Bytepanda technologies and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Delta Report"] = {
+frappe.query_reports["Financial Accounting Reports"] = {
   initial_depth: 0,
+  name_field: "ledger_code",
+  parent_field: "group",
   filters: [
     {
       fieldname: "from_date",
@@ -19,6 +21,14 @@ frappe.query_reports["Delta Report"] = {
       width: "80",
       reqd: 1,
       default: frappe.datetime.year_end(),
+    },
+    {
+      fieldname: "report",
+      label: __("Report"),
+      fieldtype: "Select",
+      options: ["Profit and Loss Statement", "Balance Sheet", "Cash Flow"],
+      default: "Balance Sheet",
+      reqd: 1,
     },
   ],
 };
